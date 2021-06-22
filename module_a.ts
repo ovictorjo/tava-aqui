a511d009546e8bbfa7e6c154e9c51189b0154367
Joao Victor <70987911+ovictorjo@users.noreply.github.com>
2021-06-22 00:03:26 -0400

    Delete module_a.ts

--- module_a.ts
+++
@@ -1,9 +0,0 @@
-import {MyDatabaseModule} from './db/my-database'
-import { MyDatabaseClassic as myDataBaseA } from './db/my-database-classic';
-
-const MyDatabaseClassic = MyDatabaseModule;
-
-MyDatabaseClassic.add({name:  'Joao', age: 12})
-MyDatabaseClassic.add({name: 'Livia', age: 22});
-MyDatabaseClassic.show();
-
